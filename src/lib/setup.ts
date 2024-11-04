af2f74e1c00dd96326f37c3c6e039e2d764c76fd
Williams Bussat <github@wbussat.fr>
2024-11-04 10:59:38 +0100

    chore: init prisma client and schema

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -1,8 +1,10 @@
 // Unless explicitly defined, set NODE_ENV as development:
 process.env.NODE_ENV ??= 'development';
 
-import { ApplicationCommandRegistries, RegisterBehavior } from '@sapphire/framework';
 import '@sapphire/plugin-logger/register';
+
+import { PrismaClient } from '@prisma/client';
+import { ApplicationCommandRegistries, container, RegisterBehavior } from '@sapphire/framework';
 import { setup } from '@skyra/env-utilities';
 import * as colorette from 'colorette';
 import { join } from 'node:path';
@@ -16,3 +18,13 @@ setup({ path: join(srcDir, '.env') });
 
 // Enable colorette
 colorette.createColors({ useColor: true });
+
+// Prisma setup
+const prisma = new PrismaClient();
+container.prisma = prisma;
+
+declare module '@sapphire/framework' {
+	interface Container {
+		prisma: PrismaClient;
+	}
+}
